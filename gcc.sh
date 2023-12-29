19c3bb0914fe0352a90ca1ffe5887f11bf4c35a0
sickbowie <developer-onizuka@example.com>
2023-12-29 12:24:26 +0000

    2023-12-29

--- gcc.sh
+++ gcc.sh
@@ -1,5 +1,6 @@
 gcc matrix.c -o matrix.o -lm
 gcc -O3 matrix_omp.c -o matrix_omp.o -lm -fopenmp 
 nvcc matrix.cu -o matrix.co -lm
+nvcc matrix_withoutWrite.cu -o matrix_withoutWrite.co -lm
 nvcc -I /usr/local/cuda/include/  -I /usr/local/cuda/targets/x86_64-linux/lib/ matrix_gds.cu -o matrix_gds.co -L /usr/local/cuda/targets/x86_64-linux/lib/ -lcufile -L /usr/local/cuda/lib64/ -lcuda -L   -Bstatic -L /usr/local/cuda/lib64/ -lcudart_static -lrt -lpthread -ldl -lcrypto -lssl
 
